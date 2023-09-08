24818192995d2f22209d35a51eb0395135bf0f03
vivcolvmn <vivcolvmn@gmail.com>
2023-09-07 20:34:31 -0700

    employee

--- test/01-create-employee-spec.js
+++ test/01-create-employee-spec.js
@@ -2,32 +2,33 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Employee = require('../employee');
+console.log(Employee, "Line 5 from test")
 
 describe('The Employee class', () => {
     let leo;
-    
+
     beforeEach(() => {
         leo = new Employee('Leonardo', 90000, 'Ninja');
     });
-    
+
     describe('constructor function', () => {
 
         it('should have a name property', () => {
             expect(leo).to.have.property('name')
         });
-        
+
         it('should have a salary property', () => {
             expect(leo).to.have.property('salary')
         });
-        
+
         it('should have a title property', () => {
             expect(leo).to.have.property('title');
         });
-        
+
         it('should have a manager property', () => {
             expect(leo).to.have.property('manager')
         });
-        
+
         it('manager property should be set to null if no "manager" argument is passed', () => {
             expect(leo.manager).to.eql(null);
         });
